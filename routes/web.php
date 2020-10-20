b224f346e8529bed4ae19dc9dbd299338a19b317
yutaka.f <remember.22.i.w@gmail.com>
2020-10-20 07:38:56 +0000

    user registration1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@
 Route::get('/', 'TasksController@index');
 
 Route::group(['middleware' => ['auth']], function () {
-  Route::resource('tasks', 'TasksController');
+Route::resource('tasks', 'TasksController');
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
